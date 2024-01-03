0729903a6c6d42f3cb6dd8b20e96923c16474dc7
Pavel Lejbov <sctzak@gmail.com>
2024-01-03 18:48:27 +0200

    add gitignore

---
+++ main.js
@@ -0,0 +1,11 @@
+// alert('Hello');
+// const answer = +prompt('Вам есть 18?', '18');
+// console.log(typeof answer);
+
+const answer = [];
+
+answer[0] = prompt('Как ваше имя?', '');
+answer[1] = prompt('Как ваша фамилия?', '');
+answer[2] = prompt('Сколько лет?', '');
+
+document.write(answer);
